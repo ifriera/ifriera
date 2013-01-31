385c4cf1642fe94e9b2c12232465c65d11c13683
ifriera <ifrieramail@gmail.com>
2013-01-31 14:51:13 -0800

    last commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifriera::Application.config.secret_token = '3b7bbb8c706b20d141e0d71c3b85d5830c3379c23c8fd54b486f1a95776128a779001e30a1d6b2edad59473b7c118ed1cdd19432282a84206742cff082943d19'
